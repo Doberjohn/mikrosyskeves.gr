d99f76128656753d050bd617206ea1baad53cfce
John Fanidis <johnfanidis@gmail.com>
2022-11-05 12:56:35 +0200

    Added search bar

--- src/components/templates/HomeTemplate/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate/HomeTemplate.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {ChangeEvent, useEffect, useState} from "react";
 import {Div} from "../../atoms";
 import {IProduct} from "../../../shared/interfaces";
 import {MinusCircleIcon, PlusCircleIcon} from "../../../shared/svgs";
@@ -10,8 +10,23 @@ interface HomeTemplateProps {
 
 export const HomeTemplate = ({products}: HomeTemplateProps) => {
    const [filteredProducts, setFilteredProducts] = useState(products);
+   const [searchTerm, setSearchTerm] = useState('');
 
-   console.log(filteredProducts)
+   useEffect(() => {
+      if (searchTerm) {
+         const newProducts = products.filter((product) => {
+            return product.title.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
+         });
+
+         if (newProducts) {
+            setFilteredProducts(newProducts);
+         } else {
+            setFilteredProducts([]);
+         }
+      } else {
+         setFilteredProducts([...products])
+      }
+   }, [searchTerm])
 
    const reduceQuantity = (product: IProduct) => {
       product.quantity = product.quantity - 1;
@@ -23,16 +38,17 @@ export const HomeTemplate = ({products}: HomeTemplateProps) => {
       setFilteredProducts([...filteredProducts])
    }
 
+   const onSearchBarChange = (event: ChangeEvent<HTMLInputElement>) => {
+      setSearchTerm(event.target.value)
+   }
+
    return (
       <Div className="container" style={{maxWidth: '1000px'}}>
          <Div className="row py-5">
+            <div className="input-group px-0 pb-3">
+               <input value={searchTerm} onChange={onSearchBarChange} type="text" className="form-control" placeholder="Αναζήτηση προϊόντος"/>
+            </div>
             <table className="table table-dark table-hover">
-               <thead>
-               <tr>
-                  <th scope="col">Όνομα Προϊόντος</th>
-                  <th className="text-center" scope="col">Ποσότητα</th>
-               </tr>
-               </thead>
                <tbody>
                {filteredProducts.map((product) => {
                   return (
